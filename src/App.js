e211f792a5d86eba18e2787fb0c537386a57d3c4
Josefine Lantz <jo.lantz@icloud.com>
2021-08-23 03:41:08 +0200

    fetch data from contract

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends React.Component {
-  render() {
+  state = { 
+    manager: "",
+    players: [],
+    balance: "",
+    value: ""
+  };
+
+  async componentDidMount() { 
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+    }
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>This contract is managed by 
+          {this.state.manager}
+          There are currently {this.state.players.length} people entered in the lottery. 
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+        <hr />
+        <form>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+            <button>Enter</button>
+          </div>
+        </form>
       </div>
     );
   }
--- src/lottery.js
+++ src/lottery.js
@@ -1,67 +1,72 @@
 import web3 from "./web3";
 
-const address = "0x967cd39c62356697af4306573EeFa10f14a59128";
+export const address = "0x967cd39c62356697af4306573EeFa10f14a59128";
 
-const abi =
-[
-  {
-    inputs: [],
-    stateMutability: 'nonpayable',
-    type: 'constructor',
-    constant: undefined,
-    payable: undefined,
-    signature: 'constructor'
-  },
-  {
-    inputs: [],
-    name: 'enter',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-    constant: undefined,
-    payable: true,
-    signature: '0xe97dcb62'
-  },
-  {
-    inputs: [],
-    name: 'getPlayers',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0x8b5b9ccc'
-  },
-  {
-    inputs: [],
-    name: 'manager',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0x481c6a75'
-  },
-  {
-    inputs: [],
-    name: 'pickWinner',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-    constant: undefined,
-    payable: true,
-    signature: '0x5d495aea'
-  },
-  {
-    inputs: [ [Object] ],
-    name: 'players',
-    outputs: [ [Object] ],
-    stateMutability: 'view',
-    type: 'function',
-    constant: true,
-    payable: undefined,
-    signature: '0xf71d96cb'
-  }
+export const abi = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "enter",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getPlayers",
+		"outputs": [
+			{
+				"internalType": "address[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinner",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "players",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ];
 
-export default new web3.eth.Contract(abi, address);
+export default new web3.eth.Contract(abi, address); 
